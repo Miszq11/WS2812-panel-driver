5a50a71d88b932ae461bc5f66d52d6727c537200
Miszq11 <michas.smolin@gmail.com>
2024-07-06 17:42:17 +0200

    fix module build BR external structure

---
+++ external/module/module_src/test.c
@@ -0,0 +1,24 @@
+//#include <linux/kernel.h>
+#include "linux/module.h"
+#include <linux/printk.h>
+
+MODULE_LICENSE("GPL-2.0");
+
+static int test_init(void);
+static void test_uninit(void);
+
+
+static int test_init(void) {
+  printk("Hello from WS2812 module\n");
+  return 0;
+}
+
+static void test_uninit(void) {
+  printk("Goodbye from WS2812 module\n");
+}
+
+
+module_init(test_init);
+module_exit(test_uninit);
+
+
